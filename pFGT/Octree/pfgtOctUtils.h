06398fe251731b4e97223c8ec8a7b2d7e3d27003
rahul.sampath <rahul.sampath@bce9b519-5a7c-11c6-7fcc-121225901d6c>
2010-03-28 03:08:38 +0000

    use 2 lists instead of flags

--- pFGT/Octree/pfgtOctUtils.h
+++ pFGT/Octree/pfgtOctUtils.h
@@ -6,7 +6,7 @@
 #include "TreeNode.h"
 #include <vector>
 
-PetscErrorCode pfgt(const std::vector<ot::TreeNode> & linOct, unsigned int maxDepth,
+PetscErrorCode pfgt(std::vector<ot::TreeNode> & linOct, unsigned int maxDepth,
     double delta, double fMag, unsigned int numFgtPtsPerProc,
     int P, int L, int K, int writeOut);
 
