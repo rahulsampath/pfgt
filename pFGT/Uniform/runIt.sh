6c3a7bf29da3bf289cf74688b27aed67e2703151
hsundar <hsundar@bce9b519-5a7c-11c6-7fcc-121225901d6c>
2010-03-28 14:02:27 +0000

    Fixed small bugs. Added corners to the sweepW2L.

--- pFGT/Uniform/runIt.sh
+++ pFGT/Uniform/runIt.sh
@@ -13,9 +13,9 @@ export epsilon=1.0e-6
 export numPtsPerProc=1000000
 export numPtsStr=1M
 
-export delta=0.015625
+export delta=0.01
 
-export numProcStr=1
+export numProcStr=8
 
 ${MPIEXEC} -n ${numProcStr} ./runUniform ${numPtsPerProc} ${fMag} ${epsilon} ${delta} ${writeOut} -log_summary >& upfgt.${numPtsStr}.${numProcStr}.txt
 
