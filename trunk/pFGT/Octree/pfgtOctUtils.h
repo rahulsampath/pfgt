2eb0bdc3a2e6895323b2fb9f182c54b438dac3a6
hsundar <hsundar@bce9b519-5a7c-11c6-7fcc-121225901d6c>
2010-03-29 16:14:27 +0000

    Added base W2L implementation to Octree.

--- trunk/pFGT/Octree/pfgtOctUtils.h
+++ trunk/pFGT/Octree/pfgtOctUtils.h
@@ -10,6 +10,11 @@ PetscErrorCode pfgt(std::vector<ot::TreeNode> & linOct, unsigned int maxDepth,
     double delta, double fMag, unsigned int numFgtPtsPerDimPerProc,
     int P, int L, int K, int writeOut);
 
+void directW2L(PetscScalar**** WlArr, PetscScalar**** WgArr, int xs, int ys, int zs, int nx, int ny, int nz, int Ne, double h, const int StencilWidth, const int PforType2, const double lambda);
+
+void sweepW2L(PetscScalar**** WlArr, PetscScalar**** WgArr, int xs, int ys, int zs, int nx, int ny, int nz, int Ne, double h, const int StencilWidth, const int PforType2, const double lambda);
+
+void directLayer(PetscScalar**** WlArr, PetscScalar**** WgArr, int xs, int ys, int zs, int nx, int ny, int nz, int Ne, double h, const int StencilWidth, const int PforType2, const double lambda);
 
 #endif
 
