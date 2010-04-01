db918bb0eced669dd2095a35314f8be360eccb88
shravan.meam <shravan.meam@bce9b519-5a7c-11c6-7fcc-121225901d6c>
2010-04-01 21:58:53 +0000

    abstract

--- trunk/pFGT/Uniform/runIt.sh
+++ trunk/pFGT/Uniform/runIt.sh
@@ -4,18 +4,18 @@ echo $PETSC_DIR
 echo $PETSC_ARCH
 
 #Replace this  
-export MPIEXEC=mpiexec
+export MPIEXEC=/cygdrive/c/petsc-3.0.0-p12/optCxx/bin/mpiexec
 
 export writeOut=0
 export fMag=10.0 
-export epsilon=1.0e-6
+export epsilon=1.0e-3
 
 export numPtsPerProc=1000000
 export numPtsStr=1M
 
 export delta=0.01
 
-export numProcStr=8
+export numProcStr=1
 
 ${MPIEXEC} -n ${numProcStr} ./runUniform ${numPtsPerProc} ${fMag} ${epsilon} ${delta} ${writeOut} -log_summary >& upfgt.${numPtsStr}.${numProcStr}.txt
 
