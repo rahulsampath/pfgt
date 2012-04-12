1025f5d17ebdba56cc6fb647b4171cdb461e67f3
rahul.sampath <rahul.sampath@bce9b519-5a7c-11c6-7fcc-121225901d6c>
2012-04-12 16:38:47 +0000

    dummy

--- trunk/pFGT/NewOctree/pfgtOctUtils.h
+++ trunk/pFGT/NewOctree/pfgtOctUtils.h
@@ -38,13 +38,15 @@ void computeFGTminsHybridExpand(std::vector<ot::TreeNode> & fgtMins, std::vector
 
 void computeFGTminsHybridDirect(std::vector<ot::TreeNode> & fgtMins, MPI_Comm comm);
 
-void computeExpandMinsHybridExpand(std::vector<ot::TreeNode> & expandMins, MPI_Comm subComm, MPI_Comm comm); 
+void computeExpandMinsHybridExpand(std::vector<ot::TreeNode> & expandMins, std::vector<ot::TreeNode> & expandTree,
+    MPI_Comm subComm, MPI_Comm comm); 
 
-void computeExpandMinsHybridDirect(std::vector<ot::TreeNode> & expandMins, MPI_Comm comm);
+void computeDirectMinsHybridDirect(std::vector<ot::TreeNode> & directMins, std::vector<ot::TreeNode> & directTree,
+    MPI_Comm subComm, MPI_Comm comm);
 
-void computeDirectMinsHybridExpand(std::vector<ot::TreeNode> & directMins, MPI_Comm comm);
+void computeExpandMinsHybridDirect(std::vector<ot::TreeNode> & expandMins, MPI_Comm subComm, MPI_Comm comm);
 
-void computeDirectMinsHybridDirect(std::vector<ot::TreeNode> & directMins, MPI_Comm subComm, MPI_Comm comm);
+void computeDirectMinsHybridExpand(std::vector<ot::TreeNode> & directMins, MPI_Comm subComm, MPI_Comm comm);
 
 #endif
 
